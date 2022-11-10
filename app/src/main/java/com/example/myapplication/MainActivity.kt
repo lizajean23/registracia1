ddcaaed17c14463e73a0550773297aa53fc319a0
User <lizi.guchua.1@btu.edu.ge>
2022-11-11 00:18:54 +0400

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import java.util.regex.Pattern.compile
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
@@ -17,42 +18,60 @@ class MainActivity : AppCompatActivity() {
     private lateinit var checkBox: CheckBox
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+                override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val actionBar: ActionBar? = supportActionBar
         actionBar?.hide()
+
+            var emailRegex = compile(
+                "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                        "\\@" +
+                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                        "(" +
+                        "\\." +
+                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                        ")+")
+            val symbols = "0123456789"
+
+
+
 
         name = findViewById(R.id.name)
         lastName = findViewById(R.id.lastName)
         email = findViewById(R.id.email)
         password = findViewById(R.id.password)
         checkBox = findViewById(R.id.checkBox)
-        button =findViewById(R.id.button)
+        button = findViewById(R.id.button)
+
 
 
             button.setOnClickListener{
-                if (name.length() < 3 ){
-                    name.error = "უნდა შეიყვანოთ მინიმუმ 3 სიმბოლო"
+                if (name.length() < 3 || !name.text.any { it.isLetter() }){
+                    name.error = "შეიყვანეთ სწორი მონაცემი "
                     return@setOnClickListener
                 }
-                if (lastName.length() < 5){
-                    lastName.error = "უნდა შეიყვანოთ მინიმუმ 5 სიმბოლო"
+                else if (lastName.length() < 5 || !lastName.text.any { it.isLetter() }){
+                    lastName.error = "შეიყვანეთ სწორი მონაცემი"
                     return@setOnClickListener
                 }
-                if ("@" !in email.text.toString() || "." !in email.text.toString()){
+                else if (!(emailRegex.matcher(email.text).matches())){
                     email.error = "შეიყვანეთ სწორი email"
                     return@setOnClickListener
+
                 }
-                if(password.length() < 8 ){
-                    password.error = "შეიყვანეთ მინიმუმ 8 სიმბოლო"
+                else if(password.length() < 8 || !(password.text.any{it in symbols})){
+                    password.error = "შეიყვანეთ მინიმუმ 8 სიმბოლო და ერთი ციფრი"
                     return@setOnClickListener
                 }
-                if(!(checkBox.isChecked)){
+                else if(!(checkBox.isChecked)){
                     checkBox.error = "გთხოვთ მონიშნეთ ველი"
                     return@setOnClickListener
 
                 }
+
 
                 Toast.makeText(this, "თქვენ წარმატებით გაიარეთ რეგისტრაცია", Toast.LENGTH_SHORT).show()
 
@@ -60,4 +79,5 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
 }
